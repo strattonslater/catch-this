6d7f1736928a3ad9286a9adcace62d0ecc24661c
GWDBIW <gabriel@daviszoo.com>
2022-04-07 19:16:23 -0700

    changed things generally

--- Assets/Scenes/PLAYERSCRIPT.cs
+++ Assets/Scenes/PLAYERSCRIPT.cs
@@ -13,6 +13,21 @@ public class PLAYERSCRIPT : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+                    if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                transform.Translate(0,+1,0);
+            }
+                    if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                transform.Translate(0,-1,0);
+            }
+                    if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.Translate(-1,0,0);
+            }
+                    if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.Translate(+1,0,0);
+            }
     }
 }
