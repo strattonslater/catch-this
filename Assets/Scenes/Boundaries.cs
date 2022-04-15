770eafc4551b0e85492424507dc2dc09df0be3df
strattonslater <95309606+strattonslater@users.noreply.github.com>
2022-04-14 20:03:01 -0700

    Revert "Revert "Boundaries added, movement sharpened""
    This reverts commit c783f0fb48a75abbb99633c4a357fb38a8117f3d.

---
+++ Assets/Scenes/Boundaries.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour {
+    public Camera MainCamera;
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+
+    // Use this for initialization
+    void Start () {
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y; //extents = size of height / 2
+    }
+
+    // Update is called once per frame
+    void LateUpdate(){
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth*3, screenBounds.x - objectWidth*3);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight/8, screenBounds.y - objectHeight);
+        transform.position = viewPos;
+    }
+}
