192ebb68b03e2f6a1e0d4a83758e444e5e4d1a08
strattonslater <95309606+strattonslater@users.noreply.github.com>
2022-04-14 18:38:11 -0700

    Changes Controller Scripts

--- Assets/Scenes/GAMECONTROLLER.cs
+++ Assets/Scenes/GAMECONTROLLER.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GAMECONTROLLER : MonoBehaviour
 {
+    [SerializeField] private GameObject PLAYER;
+    [SerializeField] private GameObject THROWER;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class GAMECONTROLLER : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Instantiate(PLAYER);
     }
 }
