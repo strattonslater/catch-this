5834d74deec8bb1a8a2f60db666d053d8102dcb4
GWDBIW <gabriel@daviszoo.com>
2022-04-28 20:24:46 -0700

    adding timer and timer script

--- Assets/Scenes/GAMECONTROLLER.cs
+++ Assets/Scenes/GAMECONTROLLER.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GAMECONTROLLER : MonoBehaviour
 {
+
+    [SerializeField] private TextMeshPro _timeText;
     [SerializeField] private GameObject PLAYER;
     [SerializeField] private GameObject THROWER;
     [SerializeField] private Camera MainCamera;
+
+    public float timeValue=60;
 
     public GameObject activePlayer;
     private Vector3 playerPosition;
@@ -22,5 +27,28 @@ public class GAMECONTROLLER : MonoBehaviour
     {
         playerPosition = activePlayer.transform.position;
         Debug.Log(playerPosition);
+
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+        }
+
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);  
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        _timeText.text = string.Format("{0:0}:{1:00}", minutes, seconds);
     }
 }
